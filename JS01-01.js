014be6bfefcc4526f9721ac0dad9227f31f80669
honeybee20192 <74129404+honeybee20192@users.noreply.github.com>
2020-11-30 21:25:26 +0900

    Update JS01-01.js

--- JS01-01.js
+++ JS01-01.js
@@ -1,1 +1,10 @@
+for(let number=1; number<=99; number++){
+  if(number%15 == 0){
+    console.log("FizzBuzz " + number);
+  }else if(number%3 == 0){
+    console.log("Fizz " + number);
+  }else if(number%5 == 0){
+    console.log("Buzz " + number);
+  }
+}
 
